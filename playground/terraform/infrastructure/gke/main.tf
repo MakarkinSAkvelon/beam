88ba3b1e8543ba1aa7c328b0f2ad9d66948b4cd8
Sergey Makarkin <yahot@mail.ru>
2023-03-10 18:05:03 +0500

    1

--- playground/terraform/infrastructure/gke/main.tf
+++ playground/terraform/infrastructure/gke/main.tf
@@ -26,7 +26,11 @@ resource "google_container_cluster" "playground-gke" {
   subnetwork                 = var.subnetwork
   remove_default_node_pool = true
 
-
+  master_authorized_networks_config {
+    cidr_block   = "10.0.0.0/8"
+    display_name = "your-display-name"
+  }
+  
   private_cluster_config {
     enable_private_endpoint = true
     enable_private_nodes    = true
